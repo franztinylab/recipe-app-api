b32f9771ca696c329a2e944bfd91f3fd4094c002
franz.tinylab@gmail.com <franz.tinylab@gmail.com>
2023-12-09 18:53:11 +0100

    Aggiunto Postgresql

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calcolatore
+"""
+
+def add(x, y):
+    """Add x to y and returns result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtracts x to y and returns result."""
+    return y - x
